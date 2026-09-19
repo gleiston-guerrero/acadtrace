10e56401fcfbb50649e2f54266ac034533799ec8
LEO23as <pcastrol@uteq.edu.ec>
2026-09-19 17:59:18 -0500

    fix(e45): retirar literal postgres del default DB_USER en settings.py de docente

--- microservicio-docente/micro_docente/settings.py
+++ microservicio-docente/micro_docente/settings.py
@@ -58,7 +58,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DB_NAME", "sga"),
-        "USER": os.environ.get("DB_USER", "postgres"),
+        "USER": os.environ.get("DB_USER", "sga_app"),
         "PASSWORD": os.environ.get("DB_PASSWORD", ""),
         "HOST": os.environ.get("DB_HOST", "192.0.2.1"),
         "PORT": os.environ.get("DB_PORT", "5433"),
